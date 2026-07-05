7a28e588f1abcc2e356d42f2074adbee06eb0d65
Eswar Maguluri <eswarmaguluri02@gmail.com>
2026-07-06 05:06:27 +0530

    Fix CORS for Vercel deployment

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://flash-link-26.vercel.app"
+        "https://flash-link-git-main-eswar5.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
